1f84ea4d1e78f82ac8da4c0972573f78ed82b4b0
Daniela Lavric <daniela@hashicorp.com>
2022-09-30 16:32:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,3 @@ resource "null_resource" "mynull" {}
 module "uses-resource" {
   source = "./modules/depends-on"
 }
-
-
-module "uses-another-resource" {
-  source = "./modules/a-submodule"
-}
-
-module "resource-again" {
-  source = "./another-module"
-}
